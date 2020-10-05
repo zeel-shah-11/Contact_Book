e169c5df38ec56e26276a22ecea54d6cca33cda1
zeel-shah-11 <72273556+zeel-shah-11@users.noreply.github.com>
2020-10-05 11:06:53 +0530

    Update ApiI.java

--- contact/src/main/java/mas/ApiI.java
+++ contact/src/main/java/mas/ApiI.java
@@ -7,11 +7,53 @@ import java.util.List;
 
 public interface ApiI {
 
-    public void  createTable(DynamoDB dynamoDB);
-    public Item queryEmail(String email, DynamoDB dynamoDB);
-    public List<String> queryName(String name, DynamoDB dynamoDB);
-    public void editItem(String email,DynamoDB dynamoDB,String name,String contact,String address);
-    public void create(String email,DynamoDB dynamoDB,String name,String contact,String address);
-    public void del(String email,DynamoDB dynamoDB);
+    /**
+     Create table in DynemoDB
+     @param  dynamoDB  the DynamoDB
+     */
+    void  createTable(DynamoDB dynamoDB);
+
+    /**
+     Query Base on email id
+     @param email emailid of required item
+     @param  dynamoDB  the DynamoDB
+     @return item base on query parameter
+     */
+    Item queryEmail(String email, DynamoDB dynamoDB);
+
+    /**
+     Query Base on name
+     @param name the name of required item
+     @param  dynamoDB  the DynamoDB
+     @return List base on query parameter
+     */
+     List<String> queryName(String name, DynamoDB dynamoDB);
+
+    /**
+     Edit item base on parameter
+     @param email the emailid
+     @param  dynamoDB  the DynamoDB
+     @param name the name
+     @param contact the contact number
+     @param address the address
+     */
+     void editItem(String email,DynamoDB dynamoDB,String name,String contact,String address);
+
+    /**
+     add item base on parameter
+     @param email the emailid
+     @param  dynamoDB  the DynamoDB
+     @param name the name
+     @param contact the contact number
+     @param address the address
+     */
+     void create(String email,DynamoDB dynamoDB,String name,String contact,String address);
+
+    /**
+     delete item base on email
+     @param email the emailid
+     @param  dynamoDB  the DynamoDB
+     */
+     void del(String email,DynamoDB dynamoDB);
 
 }
